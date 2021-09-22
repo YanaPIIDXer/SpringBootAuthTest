0cc34d03ada6c855213ffde86d72f92c352f50ee
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-23 03:45:22 +0900

    ログイン画面準備

---
+++ src/main/java/com/yanap/authtest/controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.yanap.authtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+    @RequestMapping("/admin/login")
+    public String login() {
+        return "admin/login";
+    }
+}
