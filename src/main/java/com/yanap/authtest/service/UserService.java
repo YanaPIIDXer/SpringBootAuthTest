b487070222156ae46057d33f7a1de18ab66f3435
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-23 03:13:01 +0900

    ユーザサービスクラス定義

---
+++ src/main/java/com/yanap/authtest/service/UserService.java
@@ -0,0 +1,22 @@
+package com.yanap.authtest.service;
+
+import com.yanap.authtest.user.AdminUser;
+import com.yanap.authtest.user.GeneralUser;
+import com.yanap.authtest.user.User;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
+    private GeneralUser generalUser = new GeneralUser();
+    private AdminUser adminUser = new AdminUser();
+    @Override
+    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+        User user = null;
+        if (generalUser.getUsername().equals(name)) { user = generalUser; }
+        else if (adminUser.getUsername().equals(name)) { user = adminUser; }
+        else { throw new UsernameNotFoundException("UserName Error."); }
+        return user;
+    }
+}
