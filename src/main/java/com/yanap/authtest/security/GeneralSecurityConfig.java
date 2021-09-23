aa5c231c625d9bdce45792df791bc0c8d6348eda
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-23 18:10:23 +0900

    Configurationアノテーションを付与 ※駄目

--- src/main/java/com/yanap/authtest/security/GeneralSecurityConfig.java
+++ src/main/java/com/yanap/authtest/security/GeneralSecurityConfig.java
@@ -3,6 +3,7 @@ package com.yanap.authtest.security;
 import com.yanap.authtest.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Configuration
 @Order(2)
 @EnableWebSecurity
 public class GeneralSecurityConfig extends WebSecurityConfigurerAdapter {
